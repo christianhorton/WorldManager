5957e0d4a78ae5f7681a77bc4241a4ab9a029cf2
christianhorton <christian.d.horton@gmail.com>
2015-05-16 15:51:44 -0400

    Condensed Configuration Loader

--- src/org/worldmanager/core/CorePlugin.java
+++ src/org/worldmanager/core/CorePlugin.java
@@ -1,13 +1,16 @@
 package org.worldmanager.core;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Level;
 
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,6 +28,10 @@ public class CorePlugin extends JavaPlugin  {
 	/* Configurations */
 	private FileConfiguration configConfig;
 	private FileConfiguration worldConfig;
+	
+	/* Lists */
+	public List<String> worldsCreative = new ArrayList<String>();
+	public List<String> worldsSurvival = new ArrayList<String>();
 
 	public void onEnable() {
 		currentListeners = new ArrayList<Listener>();
@@ -32,7 +39,15 @@ public class CorePlugin extends JavaPlugin  {
 		currentListeners.add(new BlockListener(this));
 		initListeners();
 		initConfigs();
+		loadConfigValues();
 		getLogger().log(Level.INFO, "Enabled");
+	
+		if(isSurvival("creative")) {
+			getLogger().log(Level.INFO, "This world is survival");
+		} else {
+			getLogger().log(Level.INFO, "This world is not survival");
+		}
+		
 	}
 
 	public void onDisable() {
@@ -53,28 +68,29 @@ public class CorePlugin extends JavaPlugin  {
 	private void initConfigs() {
 		configFile = new File(getDataFolder(), "config.yml");
 		worldFile = new File(getDataFolder(), "worlds.yml");
+		configConfig = new YamlConfiguration();
+        worldConfig = new YamlConfiguration();
 		try {
-			if(!configFile.exists()) {
-				configFile.createNewFile();
-				getLogger().log(Level.INFO, "Created config.yml");
-			} else {
-				this.configConfig.load(configFile);
-				getLogger().log(Level.INFO, "Loaded config.yml");
-				this.worldConfig.load(worldFile);
-				getLogger().log(Level.INFO, "Loaded worlds.yml");
-			}
-			if(!worldFile.exists()) { 
-				worldFile.createNewFile();
-				getLogger().log(Level.INFO, "Created worlds.yml");
-			}
-		} catch (IOException e) {
-			getLogger().log(Level.WARNING, "Could not create configuration files.");
-			e.printStackTrace();
+			this.configConfig.load(configFile);
+			getLogger().log(Level.INFO, "Loaded config.yml");
+			this.worldConfig.load(worldFile);
+			getLogger().log(Level.INFO, "Loaded worlds.yml");
 		} catch (InvalidConfigurationException e) {
 			getLogger().log(Level.WARNING, "Could not load configuration files.");
 			e.printStackTrace();
-		} 
-		
+		} catch (FileNotFoundException e) { e.printStackTrace();
+		} catch (IOException e) { e.printStackTrace(); }
+	}
 
+	private void loadConfigValues() {
+		this.worldsCreative = worldConfig.getStringList("creative");
+		this.worldsSurvival = worldConfig.getStringList("survival");
+	}
+	
+	public boolean isSurvival(String worldName) {
+		if(worldsSurvival.contains(worldName)) {
+			return true;
+		}
+		return false;
 	}
 }
