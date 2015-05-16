bceb6a316bce12419d1c0b610581a8e57888d075
christianhorton <christian.d.horton@gmail.com>
2015-05-16 15:59:04 -0400

    Added events to check world gamemodes

--- src/org/worldmanager/core/listeners/PlayerListener.java
+++ src/org/worldmanager/core/listeners/PlayerListener.java
@@ -1,19 +1,26 @@
 package org.worldmanager.core.listeners;
 
+import org.bukkit.GameMode;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.worldmanager.core.CorePlugin;
 
 public class PlayerListener implements Listener {
 
+	private CorePlugin plugin;
 
 	public PlayerListener(final CorePlugin corePlugin) {
+		this.plugin = corePlugin;
 	}
 	
+	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		Player interactPlayer = event.getPlayer();
 		World worldIn = interactPlayer.getWorld();
@@ -21,8 +28,25 @@ public class PlayerListener implements Listener {
 			if(worldIn.getName() == "world" && !interactPlayer.isOp()) {
 				event.setCancelled(true);
 			}
-		}
-				
+		}			
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		String worldName = event.getPlayer().getWorld().getName();
+		checkWorld(event.getPlayer(), worldName);
+	}
+	
+	@EventHandler
+	public void onPlayerTeleport(PlayerTeleportEvent event) {
+		String worldName = event.getTo().getWorld().getName();
+		checkWorld(event.getPlayer(), worldName);
+	}
+	
+	public void checkWorld(Player player, String worldName) {
+		if(plugin.isSurvival(worldName)) { player.setGameMode(GameMode.SURVIVAL); }
+		if(plugin.isCreative(worldName)) { player.setGameMode(GameMode.CREATIVE); }	
 	}
 
 }
+
